b190d12a36792dac354b7a4c2ba10c77dfadc63e
Cezary Kosko <cezary@ytrre.com>
2014-10-25 17:41:03 +0200

    code cleanup in firefox-addon

--- firefox-addon/lib/main.js
+++ firefox-addon/lib/main.js
@@ -1,17 +1,15 @@
+function loadLinkedIn(elem) {
+    console.log(elem);
+}
+
 var contextMenu = require('sdk/context-menu');
 var menuItem = contextMenu.Item({
   label: 'Load LinkedIn profile to PowerRecruiter',
   context: contextMenu.URLContext('https://www.linkedin.com/profile/*'),
   contentScript: 'self.on("click", function () {' +
-	    		 '    function getContentInContainer(matchClass) { ' +
-	        	 '        var elems = document.getElementsByTagName(\'*\'), i;' +
-	        	 '        for (i in elems) { ' +
-	             '            if((\' \' + elems[i].className + \' \').indexOf(\' \' + matchClass + \' \') ' +
-	             '              > -1) { ' +
-	             '                return elems[i].innerHTML; ' +
-	             '            }' +
-	             '        }' +
-	             '    }' +
-	             '    console.log(getContentInContainer(\'full-name\')) ' +
-                 '});'
+                 '    self.postMessage(document.getElementsByClassName(\'full-name\')[0].innerHTML);' +
+                 '});',
+  onMessage: function(elem) {
+    loadLinkedIn(elem);
+  }
 });
