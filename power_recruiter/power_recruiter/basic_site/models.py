0930900a2b74145d530c7fd06352ad42b8a1ce76
Krzysztof Fudali <fudali.krzysztof@gmail.com>
2014-10-25 14:22:59 +0200

    model

--- power_recruiter/power_recruiter/basic_site/models.py
+++ power_recruiter/power_recruiter/basic_site/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Attachment(models.Model):
+    pass
+
+
+class RecruitmentState(models.Model):
+    pass
+
+
+class InternetLocation(models.Model):
+    pass
+
+
+class Role(models.Model):
+    pass
+
+class Communication(models.Model):
+    pass
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_created = models.DateField
+    state = models.ForeignKey(RecruitmentState)
+
+    from_where = models.ForeignKey(InternetLocation)
+    role = models.ForeignKey(Role)
+    comm = models.ForeignKey(Communication)
+    attachments = models.ManyToManyField(Attachment)
+    caveats = models.CharField(max_length=1000)
