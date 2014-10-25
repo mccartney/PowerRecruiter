82a23c0ecd185b4e0878738c39fbbf03a3e8303f
Cezary Kosko <cezary@ytrre.com>
2014-10-25 18:07:17 +0200

    linkedin context menu logging values

--- firefox-addon/lib/main.js
+++ firefox-addon/lib/main.js
@@ -3,11 +3,11 @@ function loadLinkedIn(elem) {
 }
 
 var contextMenu = require('sdk/context-menu');
-var menuItem = contextMenu.Item({
+var linkedInMenuItem = contextMenu.Item({
   label: 'Load LinkedIn profile to PowerRecruiter',
   context: contextMenu.URLContext('https://www.linkedin.com/profile/*'),
   contentScript: 'self.on("click", function () {' +
-                 '    self.postMessage(document.getElementsByClassName(\'full-name\')[0].innerHTML);' +
+                 '    self.postMessage([document.getElementsByClassName(\'full-name\')[0].innerHTML, document.getElementsByClassName(\'profile-picture\')[0].getElementsByTagName(\'img\')[0].src, document.URL]);' +
                  '});',
   onMessage: function(elem) {
     loadLinkedIn(elem);
