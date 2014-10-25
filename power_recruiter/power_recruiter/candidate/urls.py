ce81a35b97c955a9d42c4cc0ea1c8400a155a43a
Krzysztof Fudali <fudali.krzysztof@gmail.com>
2014-10-25 19:53:41 +0200

    linki do zalacznikow

--- power_recruiter/power_recruiter/candidate/urls.py
+++ power_recruiter/power_recruiter/candidate/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, url
 
 from power_recruiter.candidate.views.candidate_json import candidate_json
+from power_recruiter.candidate.views.attachment import attachment
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', candidate_json, name='json'),
+    url(r'attachment/id=(?P<id>\d+)$', attachment, name='attachment')
 )
