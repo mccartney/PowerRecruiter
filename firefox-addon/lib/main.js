ac7ffebed7161d5f4437a68d1e6369351c008fec
Cezary Kosko <cezary@ytrre.com>
2014-10-25 18:35:34 +0200

    goldenline context menu logging values

--- firefox-addon/lib/main.js
+++ firefox-addon/lib/main.js
@@ -1,4 +1,4 @@
-function loadLinkedIn(elem) {
+function loadProfile(elem) {
     console.log(elem);
 }
 
@@ -7,9 +7,22 @@ var linkedInMenuItem = contextMenu.Item({
   label: 'Load LinkedIn profile to PowerRecruiter',
   context: contextMenu.URLContext('https://www.linkedin.com/profile/*'),
   contentScript: 'self.on("click", function () {' +
-                 '    self.postMessage([document.getElementsByClassName(\'full-name\')[0].innerHTML, document.getElementsByClassName(\'profile-picture\')[0].getElementsByTagName(\'img\')[0].src, document.URL]);' +
+                 '    self.postMessage([document.getElementsByClassName(\'full-name\')[0].innerHTML, document.getElementsByClassName(\'profile-picture\')[0].getElementsByTagName(\'img\')[0].src, document.URL]' +
+                    ');' +
                  '});',
   onMessage: function(elem) {
-    loadLinkedIn(elem);
+    loadProfile(elem);
+  }
+});
+
+var goldenLineMenuItem = contextMenu.Item({
+  label: 'Load GoldenLine profile to PowerRecruiter',
+  context: contextMenu.URLContext('http://www.goldenline.pl/*'),
+  contentScript: 'self.on("click", function () {' +
+                 '    self.postMessage([document.getElementsByClassName(\'nameSurname\')[0].innerHTML, document.getElementsByClassName(\'avatar\')[0].getElementsByTagName(\'img\')[0].src, document.URL]' +
+                    ');' +
+                 '});',
+  onMessage: function(elem) {
+    loadProfile(elem);
   }
 });
