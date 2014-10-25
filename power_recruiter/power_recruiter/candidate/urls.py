20fb79654240b412424f48d4d67458814908b980
Krzysztof Fudali <fudali.krzysztof@gmail.com>
2014-10-25 20:03:42 +0200

    cleanup

--- power_recruiter/power_recruiter/candidate/urls.py
+++ power_recruiter/power_recruiter/candidate/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, url
 
-from power_recruiter.candidate.views.candidate_json import candidate_json
-from power_recruiter.candidate.views.attachment import attachment
+from power_recruiter.candidate.views import attachment, candidate_json
 
 
 urlpatterns = patterns(
     '',
     url(r'^$', candidate_json, name='json'),
-    url(r'attachment/id=(?P<id>\d+)$', attachment, name='attachment')
+    url(r'attachment/(?P<id>\d+)$', attachment, name='attachment')
 )
