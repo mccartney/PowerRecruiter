7467984dea8ced460e2df3191bcd27e87cd5d7d2
Filip Ochnik <filip.ochnik@gmail.com>
2014-11-04 19:28:58 +0100

    PYLINT MORDA

--- power_recruiter/power_recruiter/basic_site/views/index.py
+++ power_recruiter/power_recruiter/basic_site/views/index.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from power_recruiter.candidate.models import RecruitmentState
 
+
 def index(request):
     states = RecruitmentState.objects.all()
     return render(request, "main.html", {"states": states})
--- power_recruiter/power_recruiter/candidate/views.py
+++ power_recruiter/power_recruiter/candidate/views.py
@@ -3,32 +3,53 @@ import logging
 import sys
 
 from django.http import HttpResponse, HttpResponseRedirect
-import power_recruiter.candidate.models
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
-from power_recruiter.candidate.models import Attachment, Person, RecruitmentState
+from django.views.decorators.http import require_POST
 from django import forms
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-import power_recruiter.candidate.models
+
+from power_recruiter.candidate.models import Attachment, Person, \
+    RecruitmentState
+
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        }
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO'
+    }
+}
+
+logging.config.dictConfig(LOGGING)
+
 
 class UploadFileForm(forms.ModelForm):
     class Meta:
         model = Attachment
 
-def get_attachment(request, id):
-    att = power_recruiter.candidate.models.Attachment.objects.get(pk=id)
-    return redirect(att.file.url)
+
+def get_attachment(request, attachment_id):
+    attachment = Attachment.objects.get(pk=attachment_id)
+    return redirect(attachment.file.url)
 
 
-def remove_attachment(request, id):
+def remove_attachment(request, attachment_id):
     return HttpResponse(json.dumps(1), content_type="application.json")
 
 
 def candidate_json(request):
-    persons = power_recruiter.candidate.models.Person.objects.all()
+    # TODO: this view needs to be fixed ASAP, it's a mess
+
+    persons = Person.objects.all()
 
     if request.GET.get('Awaiting contact', 0):
         persons = persons.exclude(state_id=1)
@@ -47,7 +68,7 @@ def candidate_json(request):
             {
                 'display_name': str(a),
                 'pk': a.pk
-            } for a in power_recruiter.candidate.models.Attachment.objects.filter(person_id=p.pk)
+            } for a in Attachment.objects.filter(person_id=p.pk)
         ]
         source = p.source.name
         if 'http' in p.source.name:
@@ -64,33 +85,37 @@ def candidate_json(request):
             source += '</a>'
         state = ''
         if p.state_id in [1, 3]:
-            state += '<a href="#up" id="upButton'+ str(p.pk) +'">'\
-                    '<img src="/static/img/bolt.png">' \
-                    '</a>' \
-                    '<script>$("#upButton'+ str(p.pk) +'").click(function(){' \
-                        '$.get( "candidate/up/' + str(p.pk) + '", function() {'\
-                            'reloadData();'\
-                        '});'\
-                    '});</script>'\
-                     + p.state.name +\
-                    '<a href="#down" id="downButton'+ str(p.pk) +'">'\
-                    '<img src="/static/img/boltdown.png">' \
-                    '</a>' \
-                    '<script>$("#downButton'+ str(p.pk) +'").click(function(){' \
-                        '$.get( "candidate/down/' + str(p.pk) + '", function() {'\
-                            'reloadData();'\
-                        '});'\
-                    '});</script>'
+            state += '<a href="#up" id="upButton' + str(p.pk) + '">' \
+                     '<img src="/static/img/bolt.png">' \
+                     '</a>' \
+                     '<script>$("#upButton' + str(p.pk) + \
+                     '").click(function(){' \
+                     '$.get( "candidate/up/' + str(p.pk) + '", function() {'\
+                     'reloadData();' \
+                     '});' \
+                     '});</script>'\
+                     + p.state.name + \
+                     '<a href="#down" id="downButton' + str(p.pk) + '">'\
+                     '<img src="/static/img/boltdown.png">' \
+                     '</a>' \
+                     '<script>$("#downButton' + str(p.pk) + \
+                     '").click(function(){' \
+                     '$.get( "candidate/down/' + str(p.pk) + \
+                     '", function() {' \
+                     'reloadData();' \
+                     '});' \
+                     '});</script>'
         else:
             if "Hired" in p.state.name:
-                state = "<span style='color: #419E16'>" + p.state.name + "</span>";
+                state = "<span style='color: #419E16'>" + p.state.name + \
+                        "</span>"
             else:
-                state = "<span style='color: #F00'>" + p.state.name + "</span>";
+                state = "<span style='color: #F00'>" + p.state.name + "</span>"
         resp.append({
             'id': p.pk,
             'candidate_name': p.first_name + ' ' + p.last_name,
             'source': source,
-            'state' : state,
+            'state': state,
             'comm': p.comm.name,
             'attachments': attachments,
             'caveats': p.caveats,
@@ -98,67 +123,57 @@ def candidate_json(request):
     return HttpResponse(json.dumps(resp), content_type="application/json")
 
 
+@require_POST
 @csrf_exempt
-def caveatsUpload(request):
-    if request.method == 'POST':
-        person = Person.objects.get(id=request.POST['id'])
-        person.caveats = request.POST['caveats']
-        person.save()
-        return HttpResponseRedirect(reverse('caveatsUpload'))
-    return HttpResponse("");
+def caveats_upload(request):
+    person = Person.objects.get(id=request.POST['id'])
+    person.caveats = request.POST['caveats']
+    person.save()
+    return HttpResponseRedirect(reverse('caveats_upload'))
 
 
 def upload(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            new_file = Attachment(person = Person.objects.get(id=request.POST['person']), file = request.FILES['file'])
+            person = Person.objects.get(id=request.POST['person'])
+            uploaded_file = request.FILES['file']
+            new_file = Attachment(person=person, file=uploaded_file)
             new_file.save()
-
             return HttpResponseRedirect(reverse('upload'))
     else:
         form = UploadFileForm()
-
     data = {'form': form}
-    return render_to_response('main.html', data, context_instance=RequestContext(request))
+    return render_to_response(
+        'main.html',
+        data,
+        context_instance=RequestContext(request)
+    )
 
-LOGGING = {
-    'version': 1,
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'stream': sys.stdout,
-        }
-    },
-    'root': {
-        'handlers': ['console'],
-        'level': 'INFO'
-    }
-}
-
-logging.config.dictConfig(LOGGING)
 
 def up(request, candidate_id):
-    person = power_recruiter.candidate.models.Person.objects.get(id=candidate_id)
+    person = Person.objects.get(id=candidate_id)
     person.state_id += 2
     person.save()
     return HttpResponse(200, content_type="plain/text")
 
+
 def down(request, candidate_id):
-    person = power_recruiter.candidate.models.Person.objects.get(id=candidate_id)
+    person = Person.objects.get(id=candidate_id)
     person.state_id += 1
     person.save()
     return HttpResponse(200, content_type="plain/text")
 
+
 @csrf_exempt
 def add_candidate(request):
     args = []
-    for i in ['0', '1', '2']:
-        args.append(request.POST['args['+i+']'])
+    for i in xrange(3):
+        args.append(request.POST['args[%d]' % i])
     names = args[0].split(' ')
     first_name = names[0]
-    last_name = names[len(names) - 1]
-    power_recruiter.candidate.models.Person.objects.create_person(
+    last_name = names[-1]
+    Person.objects.create_person(
         first_name,
         last_name,
         args[2]
@@ -169,8 +184,9 @@ def add_candidate(request):
 def stats(request):
     states = RecruitmentState.objects.all()
     context = {
-        'spices' : [
-            {'num' : len(Person.objects.filter(state = s.pk)), 'name' : s.name} for s in states
-        ]
+        'spices': [{
+            'num': len(Person.objects.filter(state=s.pk)),
+            'name': s.name
+        } for s in states]
     }
     return render(request, "stats.html", context)
