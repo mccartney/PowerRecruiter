28d7f9dbeacceed1de6ab3bfb6e755e92c6352b1
Filip Ochnik <filip.ochnik@gmail.com>
2014-11-22 20:28:14 +0100

    Misc fixes.

--- power_recruiter/power_recruiter/candidate/models.py
+++ power_recruiter/power_recruiter/candidate/models.py
@@ -1,15 +1,9 @@
-import datetime
+from django.utils import timezone
+from django.db.models import Manager, Model, CharField, ForeignKey, \
+    FileField, DateField, TextField, URLField, EmailField
 
-from django.db import models
 
-
-class RecruitmentState(models.Model):
-    name = models.CharField(max_length=100, default='')
-
-    def __unicode__(self):
-        return self.name
-
-class SourceManager(models.Manager):
+class SourceManager(Manager):
     def create_source(self, name):
         if "linkedin" in name:
             source = self.create(linkedin=name)
@@ -26,60 +20,55 @@ class SourceManager(models.Manager):
         source = self.create()
         return source
 
-class Source(models.Model):
-    linkedin = models.URLField(null=True, unique=True)
-    goldenline = models.URLField(null=True, unique=True)
-    email = models.EmailField(null=True, unique=True)
+
+class Source(Model):
+    linkedin = URLField(null=True, unique=True)
+    goldenline = URLField(null=True, unique=True)
+    email = EmailField(null=True, unique=True)
+
     objects = SourceManager()
 
     def __unicode__(self):
-        toReturn = ""
-        if self.linkedin is not None:
-            toReturn += self.linkedin
-        if self.goldenline is not None:
-            toReturn += self.goldenline
-        if self.lemail is not None:
-            toReturn += self.email
-        return toReturn
+        return u"".join([
+            self.linkedin or u"",
+            self.goldenline or u"",
+            self.email or u""
+        ])
 
 
-class Role(models.Model):
-    name = models.CharField(max_length=100, default='')
+class Role(Model):
+    name = CharField(max_length=100, default='')
 
     def __unicode__(self):
         return self.name
 
 
-class Communication(models.Model):
-    name = models.CharField(max_length=100, default='')
+class Communication(Model):
+    name = CharField(max_length=100, default='')
 
     def __unicode__(self):
         return self.name
 
 
-class PersonManager(models.Manager):
+class PersonManager(Manager):
     def create_person(self, first_name, last_name, source):
         person = self.create(
             first_name=first_name,
             last_name=last_name,
-            date_created=datetime.datetime.now(),
-            state=RecruitmentState.objects.get(pk=1),
             comm=Communication.objects.get(pk=1),
             source=Source.objects.create_source(source)
         )
         return person
 
 
-class Person(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_created = models.DateField()
-
-    state = models.ForeignKey(RecruitmentState)
-    source = models.ForeignKey(Source)
-    role = models.ForeignKey(Role, blank=True, null=True)
-    comm = models.ForeignKey(Communication)
-    caveats = models.TextField(max_length=1000, blank=True)
+class Person(Model):
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100)
+    date_created = DateField(default=timezone.now)
+    source = ForeignKey(Source)
+    role = ForeignKey(Role, blank=True, null=True)
+    comm = ForeignKey(Communication)
+    caveats = TextField(max_length=1000, blank=True)
 
     objects = PersonManager()
 
@@ -87,9 +76,9 @@ class Person(models.Model):
         return self.first_name + " " + self.last_name
 
 
-class Attachment(models.Model):
-    person = models.ForeignKey(Person, default=1)
-    file = models.FileField(upload_to='attachments/%Y/%m/%d')
+class Attachment(Model):
+    person = ForeignKey(Person, default=1)
+    file = FileField(upload_to='attachments/%Y/%m/%d')
 
     def __unicode__(self):
         return self.file.name[23:]
--- power_recruiter/power_recruiter/candidate/urls.py
+++ power_recruiter/power_recruiter/candidate/urls.py
@@ -7,9 +7,9 @@ from power_recruiter.candidate.views import get_attachment, \
 urlpatterns = patterns(
     '',
     url(r'^$', candidate_json, name='json'),
-    url(r'^attachment/get/(?P<id>\d+)$', get_attachment,
+    url(r'^attachment/get/(?P<attachment_id>\d+)$', get_attachment,
         name='get_attachment'),
-    url(r'^attachment/remove/(?P<id>\d+)$', remove_attachment,
+    url(r'^attachment/remove/(?P<attachment_id>\d+)$', remove_attachment,
         name='remove_attachment'),
     url(r'^add', add_candidate),
     url(r'^attachment/upload/$', upload, name='upload'),
