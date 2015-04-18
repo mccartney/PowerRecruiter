8fb64ac0ad50a16df430870a73dac14f6553c18f
Andrzej <andrzejjac@gmail.com>
2015-04-18 11:49:20 +0200

    changed runTests.sh

--- power_recruiter/power_recruiter/basic_site/static/js/jsTesting/runTests.sh
+++ power_recruiter/power_recruiter/basic_site/static/js/jsTesting/runTests.sh
@@ -1,5 +1,6 @@
 rm -rf phantom/
 phantomjs run-jscover-qunit.js http://localhost:8081/jsTesting/testView.html > qunit-report.xml
+python ./runSystemTests.py
 java -cp JSCover-all.jar jscover.report.Main --format=COBERTURAXML phantom phantom
 head -n -3 qunit-report.xml > qunit-report2.xml
 sed 's/<source>\/root\/.jenkins\/workspace\/ZPP\/power_recruiter\/power_recruiter\/basic_site\/static\/js\/jsTesting\/phantom<\/source>/<source>\/root\/.jenkins\/workspace\/ZPP\/power_recruiter\/power_recruiter\/basic_site\/static\/js\/<\/source>/g' phantom/cobertura-coverage.xml > phantom/cobertura-coverage2.xml
