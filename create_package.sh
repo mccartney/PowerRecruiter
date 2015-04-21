526e9af6c550fc5895b013dc21dc4905603a6674
Cezary Kosko <cezary@ytrre.com>
2015-04-21 23:24:47 +0200

    quasifinalny deplojment
    Former-commit-id: 53174d81d8ee82e9ad3e4dd9359901ea6648a8da

--- create_package.sh
+++ create_package.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
 
-fpm -s dir -t deb -n power-recruiter -v 0.9.0 \
+fpm -s dir -t deb -n power-recruiter -v 0.9.2 \
     -C . \
     -d "python (>=2.7)" \
     -d "python-pip" \
     -d "python-dev" \
     -d "build-essential" \
     -d "python-virtualenv" \
+    -d "gfortran" \
+    -d "libopenblas-dev" \
+    -d "liblapack-dev" \
     --prefix /opt/pr \
     --after-install after_install.sh \
     --after-upgrade after_upgrade.sh \
     -m "Filip Ochnik <filip.ochnik@gmail.com>" \
-    power_recruiter requirements.txt bin power-recruiter.conf
+    power_recruiter/power_recruiter/candidate \
+    power_recruiter/power_recruiter/basic_site \
+    power_recruiter/power_recruiter/tests \
+    power_recruiter/power_recruiter/image_comparator/*.py \
+    power_recruiter/power_recruiter/image_comparator/network.bin \
+    power_recruiter/power_recruiter/*.py \
+    power_recruiter/power_recruiter/*.pyc \
+    power_recruiter/media \
+    power_recruiter/files \
+    power_recruiter/*.sh \
+    power_recruiter/*.py \
+    requirements.txt bin power-recruiter.conf
