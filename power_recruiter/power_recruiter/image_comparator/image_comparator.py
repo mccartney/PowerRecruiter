0ddd067bc7c8ba303134f937d2c6650c8cbc5023
Filip Ochnik <filip.ochnik@gmail.com>
2015-06-27 13:18:28 +0200

    Copyright
    Former-commit-id: 56e1d4417046095492a72bb1dec258ed5f8d64e3

--- power_recruiter/power_recruiter/image_comparator/image_comparator.py
+++ power_recruiter/power_recruiter/image_comparator/image_comparator.py
@@ -1,4 +1,23 @@
-from power_recruiter.image_comparator.neural_network import network, get_input_image
+"""
+Power Recruiter - a browser-based FSM-centered database application profiled for IT recruiters
+Copyright (C) 2015 Krzysztof Fudali, Andrzej Jackowski, Cezary Kosko, Filip Ochnik
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from power_recruiter.image_comparator.neural_network import network, \
+    get_input_image
 
 
 def is_same_person(path1, path2):
@@ -6,7 +25,4 @@ def is_same_person(path1, path2):
     if pre_results:
         return True
     score_one = network.network.activate(input_image)
-    #print >> sys.stderr,"SCORE 1"
-    #print >> sys.stderr,score_one[0]
-    #print >> sys.stderr,score_one[1]
     return score_one[0] > score_one[1]
